72559c12e94ef33077dcbef0909143586f09aaf4
franek86 <frane.konjevoda@gmail.com>
2026-03-11 12:21:39 +0100

    Set vite config for production

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       "/api": {
         target: "https://fivet-backend.onrender.com/",
         changeOrigin: true,
-        secure: false,
+        secure: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
